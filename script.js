22288cfbb085169a74548c0e52b687e3f39b4248
Scidoria <95399483+Scidoria@users.noreply.github.com>
2024-06-08 02:41:20 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 let img_array = [
-  "https://raw.githubusercontent.com/Scidoria/firstProjectTest/main/path/to/0.jpg",
-  "https://raw.githubusercontent.com/Scidoria/firstProjectTest/main/path/to/1.jpg",
-  "https://raw.githubusercontent.com/Scidoria/firstProjectTest/main/path/to/2.jpg",
-"https://raw.githubusercontent.com/Scidoria/firstProjectTest/main/path/to/3.jpg"
+  "https://raw.githubusercontent.com/Scidoria/firstProjectTest/main/Images/0.jpg",
+  "https://raw.githubusercontent.com/Scidoria/firstProjectTest/main/Images/1.jpg",
+  "https://raw.githubusercontent.com/Scidoria/firstProjectTest/main/Images/2.jpg",
+"https://raw.githubusercontent.com/Scidoria/firstProjectTest/main/Images/3.jpg"
 ];
 const main = document.getElementById("main");
 const button = document.getElementById("changeButton");
